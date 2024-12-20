25c59415106cfaf278e2482b658d1a83ebd93221
farhanizza <farhanizza46@gmail.com>
2024-12-20 09:46:43 +0700

    feat: new feature

--- client/src/Components/Icon/IconLove.js
+++ client/src/Components/Icon/IconLove.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function IconLove() {
+	const [click, setClick] = useState(false);
 	return (
 		<svg
 			width="32"
@@ -8,13 +9,16 @@ export default function IconLove() {
 			viewBox="0 0 32 32"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
-			className="cursor-pointer"
+			className="cursor-pointer transition-colors duration-300"
+			onClick={() => setClick((prevClick) => !prevClick)}
 		>
 			<path
 				fill-rule="evenodd"
 				clip-rule="evenodd"
 				d="M6.04791 7.70398C7.2981 6.45418 8.99348 5.75208 10.7612 5.75208C12.529 5.75208 14.2244 6.45418 15.4746 7.70398C15.719 7.94754 16.0337 8.25065 16.4186 8.61332C16.8026 8.25065 17.1168 7.94754 17.3612 7.70398C18.6062 6.46488 20.2899 5.76716 22.0463 5.76246C23.8028 5.75776 25.4902 6.44644 26.7418 7.67886C27.9933 8.91128 28.7079 10.5878 28.7303 12.3442C28.7527 14.1005 28.081 15.7948 26.8612 17.0587L17.3599 26.56C17.1099 26.8099 16.7708 26.9504 16.4172 26.9504C16.0637 26.9504 15.7246 26.8099 15.4746 26.56L5.97324 17.06C4.75463 15.8032 4.07926 14.1175 4.09298 12.367C4.1067 10.6164 4.80975 8.94153 6.04791 7.70398Z"
-				fill="#34364A"
+				className={`transition-colors duration-300 ${
+					click ? 'fill-[#FF8383]' : 'fill-[#34364A]'
+				}`}
 			/>
 		</svg>
 	);
--- client/src/pages/Home/Profile.js
+++ client/src/pages/Home/Profile.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Navbar from '../../Components/Pages/Navbar';
 import Button from '../../Components/Auth/Button';
+import figma from '../../assets/image/figma.png';
+import IconLove from '../../Components/Icon/IconLove';
+import IconFileImage from '../../Components/Icon/IconFileImage';
 
 export default function Profile() {
+	const fileInputRef = useRef(null);
+	const [backgroundImageUrl, setBackgroundImageUrl] = useState(null);
+
+	const dataHome = Array.from({ length: 5 });
+
+	const handleButtonClick = () => {
+		fileInputRef.current.click();
+	};
+	const handleFileChange = (event) => {
+		const file = event.target.files[0];
+		if (file) {
+			const imageUrl = URL.createObjectURL(file);
+			setBackgroundImageUrl(imageUrl);
+		}
+	};
+
+	const handleUpdateProfile = () => {};
 	return (
 		<>
 			<Navbar name={'Han'} />
@@ -10,51 +30,104 @@ export default function Profile() {
 				<div className="flex flex-col">
 					<p className="text-[24px] font-bold text-[#34364A]">Profile</p>
 					<div className="flex flex-col mt-[57px] space-y-[13px]">
-						<div className="flex flex-col">
-							<label
-								htmlFor="username"
-								className="text-[18px] font-medium text-[#34364A]"
-							>
-								Username
-							</label>
-							<input
-								type="text"
-								name="username"
-								id="username"
-								className="w-[409px] h-[45px] rounded-[12px] border-[3px] border-[#EDDCFC] mt-[13px]"
-							/>
-						</div>
-						<div className="flex flex-col">
-							<label
-								htmlFor="email"
-								className="text-[18px] font-medium text-[#34364A]"
-							>
-								Email
-							</label>
-							<input
-								type="text"
-								name="email"
-								id="email"
-								className="w-[409px] h-[45px] rounded-[12px] border-[3px] border-[#EDDCFC] mt-[13px]"
-							/>
-						</div>
-						<div className="flex flex-col">
-							<label
-								htmlFor="password"
-								className="text-[18px] font-medium text-[#34364A]"
-							>
-								Password
-							</label>
-							<input
-								type="password"
-								name="password"
-								id="password"
-								className="w-[409px] h-[45px] rounded-[12px] border-[3px] border-[#EDDCFC] mt-[13px]"
-							/>
+						<div className="flex">
+							<div className="flex flex-col space-y-[13px]">
+								<div className="flex flex-col">
+									<label
+										htmlFor="username"
+										className="text-[18px] font-medium text-[#34364A]"
+									>
+										Username
+									</label>
+									<input
+										type="text"
+										name="username"
+										id="username"
+										className="w-[409px] h-[45px] rounded-[12px] border-[3px] border-[#EDDCFC] mt-[13px] pl-[10px]"
+									/>
+								</div>
+								<div className="flex flex-col">
+									<label
+										htmlFor="email"
+										className="text-[18px] font-medium text-[#34364A]"
+									>
+										Email
+									</label>
+									<input
+										type="text"
+										name="email"
+										id="email"
+										className="w-[409px] h-[45px] rounded-[12px] border-[3px] border-[#EDDCFC] mt-[13px] pl-[10px]"
+									/>
+								</div>
+								<div className="flex flex-col">
+									<label
+										htmlFor="password"
+										className="text-[18px] font-medium text-[#34364A]"
+									>
+										Password
+									</label>
+									<input
+										type="password"
+										name="password"
+										id="password"
+										className="w-[409px] h-[45px] rounded-[12px] border-[3px] border-[#EDDCFC] mt-[13px] pl-[10px]"
+									/>
+								</div>
+							</div>
+							<div className="flex items-center -mt-[20px]">
+								<div className="absolute">
+									<div className="w-[350px] h-[350px] overflow-hidden ml-[421px] rounded-full">
+										<img
+											src={backgroundImageUrl ? backgroundImageUrl : figma}
+											alt=""
+											className="w-full h-full object-cover"
+										/>
+									</div>
+									<div className="flex justify-end -mt-10 mr-16 cursor-pointer">
+										<div
+											className="w-[32px] h-[32px] flex items-center justify-center bg-[#EDDCFC] rounded-full"
+											onClick={handleButtonClick}
+										>
+											<IconFileImage />
+										</div>
+										<input
+											ref={fileInputRef}
+											type="file"
+											accept="image/*"
+											style={{ display: 'none' }}
+											onChange={handleFileChange}
+										/>
+									</div>
+								</div>
+							</div>
 						</div>
 						<div className="mt-[53px]">
-							<Button name={'Update profile'} />
+							<Button
+								name={'Update profile'}
+								action={() => handleUpdateProfile()}
+							/>
 						</div>
+					</div>
+					<div className="flex items-center mt-[120px]">
+						<p className="font-bold text-[24px] text-[#34364A] mr-[19px]">
+							Saved Template
+						</p>
+						<IconLove />
+					</div>
+					<div className="flex mt-[32px] space-x-[45px]">
+						{dataHome.map((_, index) => (
+							<div
+								className="w-[200px] h-[140px] overflow-hidden rounded-[8px]"
+								key={index}
+							>
+								<img
+									src={figma}
+									alt=""
+									className="w-full h-full object-cover"
+								/>
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
